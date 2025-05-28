3f57735c52c4e7c08c33dd14feb67bd2eb001cce
gymynnym <gymynnym@gmail.com>
2025-05-28 23:35:59 +0900

    feat(auth): Implement JWT and pre-shared token authenticate

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
-import { Elysia } from 'elysia';
+import { Elysia, status } from 'elysia';
+import jwt from '@elysiajs/jwt';
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000);
+const app = new Elysia()
+  .use(jwt({ name: 'jwt', alg: 'HS512', secret: Bun.env.JWT_SECRET!! }))
+  .derive(({ jwt, headers }) => {
+    const authHeader = headers['authorization'];
+    if (!authHeader?.startsWith('Bearer ')) {
+      return status(401, 'Unauthorized');
+    }
+
+    const token = authHeader.slice(7); // Remove Bearer
+
+    if (token === Bun.env.INTERNAL_TOKEN) {
+      return { isAuth: true };
+    }
+
+    try {
+      const payload = jwt.verify(token);
+      return { isAuth: true, payload };
+    } catch {
+      return status(401, 'Invalid Token');
+    }
+  })
+  .get('/', () => 'Hello Elysia')
+  .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
