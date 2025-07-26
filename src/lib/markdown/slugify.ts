9c622ccac069149a3782e78b0787c80bd7012472
gymynnym <gymynnym@gmail.com>
2025-07-26 23:53:08 +0900

    refactor(markdown): Integrate loghub-markdown-renderer

--- src/lib/markdown/slugify.ts
+++
@@ -1,9 +0,0 @@
-export const slugify = (text: string) =>
-  encodeURIComponent(
-    text
-      .toLowerCase()
-      .replace(/%20/g, '-')
-      .replace(/[^가-힣ㄱ-ㅎㅏ-ㅣa-z0-9-_]/g, '-')
-      .replace(/--+/g, '-')
-      .replace(/^-|-$/g, '')
-  );
--- src/modules/markdown/service.ts
+++ src/modules/markdown/service.ts
@@ -1,11 +1,17 @@
-import { parseMarkdown } from '~/lib/markdown/parse';
+import { MarkdownRenderer } from 'loghub-markdown-parser';
 
 export abstract class MarkdownService {
+  private static renderer = new MarkdownRenderer({
+    useMarkdownItAnchor: true,
+    useSafeLinkify: true,
+    useSanitize: true,
+  });
+
   static async parse(markdown: string | string[]) {
     if (typeof markdown === 'string') {
-      return { html: parseMarkdown(markdown) };
+      return { html: this.renderer.parse(markdown) };
     }
 
-    return { html: markdown.map((md) => parseMarkdown(md)) };
+    return { html: markdown.map((md) => this.renderer.parse(md)) };
   }
 }
