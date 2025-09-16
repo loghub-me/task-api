cdcd3136b363e2175db63f4968dd0fac1f939d07
gymynnym <gymynnym@gmail.com>
2025-09-16 16:53:10 +0900

    feat(markdown): Add anchor parsing with jsdom
    - Add dependency `jsdom` for anchor parsing

--- src/modules/markdown/service.ts
+++ src/modules/markdown/service.ts
@@ -1,4 +1,5 @@
 import { MarkdownRenderer } from 'loghub-markdown-renderer';
+import { JSDOM } from 'jsdom';
 
 export abstract class MarkdownService {
   private static renderer = new MarkdownRenderer({
@@ -9,9 +10,27 @@ export abstract class MarkdownService {
 
   static async render(markdown: string | string[]) {
     if (typeof markdown === 'string') {
-      return { result: this.renderer.render(markdown) };
+      return { result: this.renderAndParseAnchors(markdown) };
     }
 
-    return { result: markdown.map((md) => this.renderer.render(md)) };
+    return { result: markdown.map((markdown) => this.renderAndParseAnchors(markdown)) };
+  }
+
+  private static renderAndParseAnchors(markdown: string) {
+    const html = this.renderer.render(markdown);
+    const dom = new JSDOM(html);
+    const anchors = this.parseAnchors(dom.window.document.body.innerHTML);
+    return { html, anchors };
+  }
+
+  private static parseAnchors(html: string) {
+    const dom = new JSDOM(html);
+    return Array.from(dom.window.document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]')).map(
+      (el) => ({
+        level: parseInt(el.tagName[1], 10),
+        slug: el.id,
+        text: el.textContent || '',
+      })
+    );
   }
 }
