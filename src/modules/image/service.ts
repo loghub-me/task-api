1641dc04c1bab5fb1721226f51c0e17e056ba835
gymynnym <gymynnym@gmail.com>
2025-10-22 11:19:12 +0900

    fix(image): Sanitize filename before upload
    - Bump version to `0.1.3`

--- src/modules/image/service.ts
+++ src/modules/image/service.ts
@@ -6,7 +6,10 @@ export abstract class ImageService {
       throw new Error('Invalid file type');
     }
 
-    const filename = file.name.replace(/\.[^/.]+$/, '');
+    const filename = file.name
+      .replace(/\.[^/.]+$/, '') // 확장자 제거
+      .replace(/[^a-zA-Z0-9_-]/g, '') // 허용되지 않은 문자 제거
+      .substring(0, 20); // 최대 길이 20자
     const path = `${userId}/${Date.now()}_${filename}.webp`;
     await convertToWebpAndUploadImage(file, path);
     return { path };
